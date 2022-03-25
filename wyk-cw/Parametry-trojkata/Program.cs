ace11821423e004475f07b24e1d4107c36c6d4ed
Marek Porębski <marek.bp@poczta.fm>
2022-03-25 12:17:56 +0100

    Parametry-trojkata

--- wyk-cw/Parametry-trojkata/Program.cs
+++ wyk-cw/Parametry-trojkata/Program.cs
@@ -1,42 +1,104 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Parametry_trojkata
 {
     class Program
     {
-        static void Main(string[] args)
+        //WARUNEK BUDOWY TRÓJKĄTA
+        public static void Triangle(double[] sides, out bool isTriangle)
         {
-            Console.WriteLine("Podaj dlugosc boku a; b; c");
-            string readSides = Console.ReadLine();
-            //TODO: zaokraglenie do 2 miejsc po przecinku
-            double[] sides = Array.ConvertAll<string, double>(readSides.Split("; "), double.Parse);
-            //WARUNEK BUDOWY TRÓJKĄTA
             //warunek długości boków dodatnie
-            foreach (var s in sides)
+            foreach (var side in sides)
             {
-                if(s < 0)
-                    Console.WriteLine("Błędne dane. Długości boków muszą być dodatne!");
+                if (side <= 0)
+                {
+                    Console.WriteLine("Błędne dane. Długości boków muszą być dodatnie!");
+                    isTriangle = false;
+                    return;
+                }
             }
             //warunek ilość boków
             if (sides.Length <= 2)
+            {
                 Console.WriteLine("Błędne dane. Trójkąt wymaga 3 boków!");
-            if(sides.Length >= 4)
+                isTriangle = false;
+            }
+            else if (sides.Length >= 4)
+            {
                 Console.WriteLine("Błędne dane. Za dużo boków!");
+                isTriangle = false;
+            }
             //warunek suma długści boków
-            if(sides[0] + sides[1]<= sides[2] && sides[1] + sides[2] <= sides[0] && sides[2] + sides[0] <= sides[1])
+            else if (sides[0] + sides[1] < sides[2] || sides[1] + sides[2] < sides[0] || sides[2] + sides[0] < sides[1])
+            {
                 Console.WriteLine("Błędne dane. Trójkąta nie można zbudować!");
+                isTriangle = false;
+            }
+            else
+                isTriangle = true;
+        }
+
+        static void Main(string[] args)
+        {
+            string readSides = Console.ReadLine();
+            //TODO: zaokraglenie do 2 miejsc po przecinku
+            double[] sides = Array.ConvertAll<string, double>(readSides.Split("; "), double.Parse);
+            bool isTriangle;
+
+            Triangle(sides, out isTriangle);
 
             //jeśli dane poprawne
-            //obwód
-            double perimeter = 0;
-            foreach(var s in sides)
+            if (isTriangle)
             {
-                perimeter += s;
+                //obwód
+                double perimeter = 0;
+                foreach (var s in sides)
+                {
+                    perimeter += s;
+                }
+                perimeter = Math.Round(perimeter, 2);
+                Console.WriteLine($"obwód = {perimeter:F2}");
+
+                //pole
+                double area = 0;
+                double p = 0;
+                foreach (var side in sides)
+                {
+                    p += side;
+                }
+                p /= 2;
+                area = Math.Sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
+                area = Math.Round(area, 2);
+                Console.WriteLine($"pole = {area:F2}");
+
+                //typ
+                //ostrokątny
+                if (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) < Math.Pow(sides[2], 2)
+                    || Math.Pow(sides[1], 2) + Math.Pow(sides[2], 2) < Math.Pow(sides[0], 2)
+                    || Math.Pow(sides[2], 2) + Math.Pow(sides[0], 2) < Math.Pow(sides[1], 2))
+                {
+                    Console.WriteLine("trójkąt jest rozwartokątny");
+                    if(sides[0] == sides[1] || sides[1] == sides[2] || sides[0] == sides[2])
+                        Console.WriteLine("trójkąt równoramienny");
+                }
+                //prostokątny
+                else if (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2)
+                        || Math.Pow(sides[1], 2) + Math.Pow(sides[2], 2) == Math.Pow(sides[0], 2)
+                        || Math.Pow(sides[2], 2) + Math.Pow(sides[0], 2) == Math.Pow(sides[1], 2))
+                    Console.WriteLine("trójkąt jest prostokątny");
+                //rozwartokątny
+                else if (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) > Math.Pow(sides[2], 2)
+                        || Math.Pow(sides[1], 2) + Math.Pow(sides[2], 2) > Math.Pow(sides[0], 2)
+                        || Math.Pow(sides[2], 2) + Math.Pow(sides[0], 2) > Math.Pow(sides[1], 2))
+                {
+                    Console.WriteLine("trójkąt jest ostrokątny");
+                    if (sides[0] == sides[1] && sides[1]== sides[2] && sides[0] == sides[2])
+                        Console.WriteLine("trójkąt równoboczny");
+                    else if (sides[0] == sides[1] || sides[1] == sides[2] || sides[0] == sides[2])
+                        Console.WriteLine("trójkąt równoramienny");
+                }
             }
-            perimeter = Math.Round(perimeter, 2);
-            Console.WriteLine($"Perimeter: {perimeter}");
-            //pole
-            double area = 0;
         }
     }
 }
