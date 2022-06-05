e09537f2d9d6a43b18f8cd7d6e3907bc121a1171
Marek Porębski <marek.bp@poczta.fm>
2022-06-05 19:06:59 +0200

    WPF-app2

--- App-1/MainWindow.xaml.cs
+++ App-1/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,8 +26,47 @@ namespace App_1
     /// </summary>
     public partial class MainWindow : Window
     {
-        record Rate(string Code, string Currency, double Bid, double Ask);
+        record Rate()
+        {
+            [JsonPropertyName("code")]
+            public string Code { get; set; }
+            [JsonPropertyName("currency")]
+            public string Currency { get; set; }
+            [JsonPropertyName("bid")]
+            public decimal Bid { get; set; }
+            [JsonPropertyName("ask")]
+            public decimal Ask { get; set; }
+        };
         Dictionary<string, Rate> Rates = new Dictionary<string, Rate>();
+        class RateTable
+        {
+            [JsonPropertyName("table")]
+            public string Table { get; set; }
+            [JsonPropertyName("no")]
+            public string Number { get; set; }
+            [JsonPropertyName("tradingDate")]
+            public DateTime tradingDate { get; set; }
+            [JsonPropertyName("effectiveDate")]
+            public DateTime efectiveDate { get; set; }
+            [JsonPropertyName("rates")]
+            public List<Rate> Rates { get; set; }
+        }
+
+        private void DownloadDataJson()
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("Accept", "application/json");
+            string json = client.DownloadString("https://api.nbp.pl/api/exchangerates/tables/C");
+            RateTable ratesTable = JsonSerializer.Deserialize<List<RateTable>>(json)[0];
+            ratesTable.Rates.Add(new Rate() { Code = "PLN", Currency = "złoty", Ask = 1, Bid = 1 });
+
+
+            foreach (Rate r in ratesTable.Rates)
+            {
+                Rates.Add(r.Code,r);
+            }
+        }
+
         private void DownloadData()
         {
             //stworzyc kolekcje rates
@@ -36,12 +79,13 @@ namespace App_1
                 .Elements("ExchangeRatesTable")
                 .Elements("Rates")
                 .Elements("Rate")
-                .Select(node => new Rate(
-                    node.Element("Code").Value,
-                    node.Element("Currency").Value,
-                    0,//double.Parse(node.Element("Bid").Value),
-                    0//double.Parse(node.Element("Ask").Value)
-                    )).ToList();
+                .Select(node => new Rate() { 
+                    Code = node.Element("Code").Value,
+                    Currency = node.Element("Currency").Value,
+                    Bid = decimal.Parse(node.Element("Bid").Value),
+                    Ask = decimal.Parse(node.Element("Ask").Value)
+                    }
+                ).ToList();
 
             foreach(Rate r in list)
             {
@@ -53,19 +97,20 @@ namespace App_1
         {
             InitializeComponent();
 
-            DownloadData();
-            //dodawanie do listy
-            foreach(string code in Rates.Keys)
+            DownloadDataJson();
+
+            UpdateGUI();
+        }
+
+        private void UpdateGUI()
+        {
+            InputCurrencyCode.Items.Clear();
+            ResultCurrencyCode.Items.Clear();
+            foreach (string code in Rates.Keys)
             {
                 InputCurrencyCode.Items.Add(code);
                 ResultCurrencyCode.Items.Add(code);
             }
-            //InputCurrencyCode.Items.Add("USD");
-            //InputCurrencyCode.Items.Add("PLN");
-            //InputCurrencyCode.Items.Add("EUR");
-            //ResultCurrencyCode.Items.Add("USD");
-            //ResultCurrencyCode.Items.Add("PLN");
-            //ResultCurrencyCode.Items.Add("EUR");
 
             //ustawienie wartosci domyslnej
             ResultCurrencyCode.SelectedIndex = 0;
@@ -89,21 +134,63 @@ namespace App_1
             string inputCode = (string)InputCurrencyCode.SelectedItem;
             string resultCode = (string)ResultCurrencyCode.SelectedItem;
             string amountStr = InputValue.Text;
-            MessageBox.Show($"Wybrany kod wejściowy: {inputCode}\rWybrany kod wyjściowy: {resultCode}\nKwota: {amountStr}");
+            if(decimal.TryParse(amountStr, out decimal amount))
+            {
+                ResultValue.Text = (amount * Rates[inputCode].Bid / Rates[resultCode].Bid).ToString("N2");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Wybierz plik z notowania walut";
+            dialog.Filter = "Pliki tekstowe (*.txt)|*.txt";
+            dialog.DefaultExt = "*.txt";
+
+            if(dialog.ShowDialog() == true)
+            {
+                string path = dialog.FileName;
+                if (File.Exists(path))
+                {
+                    string[] lines = File.ReadAllLines(path);
+                    Rates.Clear();
+                    foreach(string line in lines)
+                    {
+                        string[] tokens = line.Split("\t");
+                        //cos sie wywala
+                        Rate rate = new Rate()
+                        {
+                            Code = tokens[0],
+                            Currency = tokens[1],
+                            Ask = decimal.Parse(tokens[2]),
+                            Bid = decimal.Parse(tokens[3])
+                        };
+                        Rates.Add(rate.Code, rate);
+                    }
+                    UpdateGUI();
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            if(dialog.ShowDialog() == true)
+            {
+                string jsonText = JsonSerializer.Serialize <Dictionary<string, Rate>>(Rates);
+                File.WriteAllText(dialog.FileName, jsonText);
+            }
+        }
+
+        private void InputValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
 
         private void InputValue_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             //spawdzenie czy input to liczba
-            if (e.Text.EndsWith(","))
-            {
-                string ee = e.Text + "0";
-                e.Handled = !decimal.TryParse(ee, out decimal value);
-            }
-            else
-            {
-                e.Handled = !decimal.TryParse(e.Text, out decimal value);
-            }
+            e.Handled = !decimal.TryParse(InputValue.Text + e.Text, out decimal value);
         }
     }
 }
--- abc/Program.cs
+++ abc/Program.cs
@@ -1,100 +1,236 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
+using System.Drawing;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace abc
+class App
 {
-    class Program
+    public static void Main(string[] args)
     {
-        /*private static int Printer(int j)
+        Car[] cars = new Car[]
         {
-            for (var i = j; i > 0; i = Printer(i - 1))
-            {
-                Console.Write(i);
-            }
-            return j;
-        }*/
-             
-
-        public static void Main(string[] args)
-        {
-            /*Printer(2);*/
-
-            /* // */
-
-
-            /*int[] n;
-
-            n.GetUpperBound(0)+1;
-            n.LongLength;*/
-
-
-            /*int n = 2;
-            for (int i = 1; i <= 6; i++){
-                for (int j = 1; j <= 6; j++){
-                    if (i != j) { n--; }
-                }
-            }
-            Console.WriteLine(n);*/
-
-
-            /*Console.WriteLine(1/2==1/3);*/
-            /*Console.WriteLine('1'+'2'=="12");
-            /*Console.WriteLine((0, 0) == (0, 0));*/
-            /*Console.WriteLine(!!true);*/
-            /*Console.WriteLine(' ' == 32);*/
-            /* Console.WriteLine(false || true);*/
-            /*Console.WriteLine(' ' == " ");*/
-            /*Console.WriteLine(null == new object());*/
-            /*Console.WriteLine(true == 1);*/
-            /*Console.WriteLine(2f+2d==4);*/
-            /*Console.WriteLine((new object()) == (new object()));*/
-
-
-            /*int fun(int n)
-            {
-                if (n < 2) return n;
-                *//*if (n % 2 == 0) return fun(n - 3) + 1;*//*
-                else return fun(n - 2);
-            }
-            Console.WriteLine(fun(6));*/
-
-
-            /*int[] a = new int[] { 5, 6, 7, 8 };
-            int[] b = new int[] { 1, 2, 3, 4 };
-
-            Stack<int> S = new Stack<int>(a);
-            Queue<int> Q = new Queue<int>(b);
-
-            S.Push(Q.Peek()); Q.Dequeue();
-            S.Push(Q.Peek()); Q.Dequeue();
-            S.Push(Q.Peek()); Q.Dequeue();
-            Q.Enqueue(S.Peek()); S.Pop();
-            Q.Enqueue(S.Peek()); S.Pop();
-            S.Push(Q.Peek()); Q.Dequeue();
-            
-
-            foreach (var sex in S)
-            {
-                Console.WriteLine(sex);
-            }
-            Console.WriteLine();
-            foreach (var qtz in Q)
-            {
-                Console.WriteLine(qtz);
-            }*/
-        }
-        /*public class ThisUsage
-        {
-            int planets;
-            static int suns;
-
-            public void gaze()
-            {
-                int i;
-                i = this.suns;
-            }
-
-        }*/
+             new Car(),
+             new Car(Model: "Fiat", true),
+             new Car(),
+             new Car(Power: 100),
+             new Car(Model: "Fiat", true),
+             new Car(Power: 125),
+             new Car()
+            };
+        Console.WriteLine(Exercise3.CarCounter(cars));
     }
+}
 
+enum Direction8
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UP_LEFT,
+    DOWN_LEFT,
+    UP_RIGHT,
+    DOWN_RIGHT
+}
+
+enum Direction4
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+//Cwiczenie 1
+//Zdefiniuj metodę NextPoint, która powinna zwracać krotkę ze współrzędnymi piksela przesuniętego jednostkowo w danym kierunku względem piksela point.
+//Krotka screenSize zawiera rozmiary ekranu (width, height)
+//Przyjmij, że początek układu współrzednych (0,0) jest w lewym górnym rogu ekranu, a prawy dolny ma współrzęne (witdh, height) 
+//Pzzykład
+//dla danych wejściowych
+//(int, int) point1 = (2, 4);
+//Direction4 dir = Direction4.UP;
+//var point2 = NextPoint(dir, point1);
+//w point2 powinny być wartości (2, 3);
+//Jeśli nowe położenie jest poza ekranem to metoda powinna zwrócić krotkę z point
+class Exercise1
+{
+    public static (int, int) NextPoint(Direction4 direction, (int, int) point, (int, int) screenSize)
+    {
+        return direction switch
+        {
+            Direction4.UP => (point.Item2 + 1, point.Item1),
+            Direction4.DOWN => (point.Item2 - 1, point.Item1),
+            Direction4.LEFT => (point.Item2, point.Item1 - 1),
+            Direction4.RIGHT => (point.Item2, point.Item1 + 1),
+            _ => point
+        };
+    }
+}
+//Cwiczenie 2
+//Zdefiniuj metodę DirectionTo, która zwraca kierunek do piksela o wartości value z punktu point. W tablicy screen są wartości
+//pikseli. Początek układu współrzędnych (0,0) to lewy górny róg, więc punkt o współrzęnych (1,1) jest powyżej punktu (1,2) 
+//Przykład
+// Dla danych weejsciowych
+// static int[,] screen =
+// {
+//    {1, 0, 0},
+//    {0, 0, 0},
+//    {0, 0, 0}
+// };
+// (int, int) point = (1, 1);
+// po wywołaniu - Direction8 direction = DirectionTo(screen, point, 1);
+// w direction powinna znaleźć się stała UP_LEFT
+class Exercise2
+{
+    static int[,] screen =
+    {
+        {1, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+
+    private static (int, int) point = (1, 1);
+
+    private Direction8 direction = DirectionTo(screen, point, 1);
+
+    public static Direction8 DirectionTo(int[,] screen, (int, int) point, int value)
+    {
+        var length = screen.GetLength(0);
+        var newpoint = (1, 1);
+        for (int j = 0; j < 3; j++)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (screen[i, j] == 1) { newpoint.Item1 = i; newpoint.Item2 = j; }
+            }
+        }
+        Direction8 wynik = Direction8.DOWN;
+        if (newpoint.Item1 - point.Item1 < 0)
+        {
+            if (newpoint.Item2 - point.Item2 < 0)
+            {
+                wynik = Direction8.UP_LEFT;
+            }
+            else if (newpoint.Item2 - point.Item2 > 0)
+            {
+                wynik = Direction8.DOWN_LEFT;
+            }
+            else
+            {
+                wynik = Direction8.LEFT;
+            }
+        }
+        else if (newpoint.Item1 - point.Item1 > 0)
+        {
+            if (newpoint.Item2 - point.Item2 < 0)
+            {
+                wynik = Direction8.UP_RIGHT;
+            }
+            else if (newpoint.Item2 - point.Item2 > 0)
+            {
+                wynik = Direction8.DOWN_RIGHT;
+            }
+            else
+            {
+                wynik = Direction8.RIGHT;
+            }
+        }
+        else
+        {
+            if (newpoint.Item2 - point.Item2 > 0)
+            {
+                wynik = Direction8.UP;
+            }
+            else if (newpoint.Item2 - point.Item2 < 0)
+            {
+                wynik = Direction8.DOWN;
+            }
+
+        }
+
+        return wynik;
+    }
+}
+
+//Cwiczenie 3
+//Zdefiniuj metodę obliczającą liczbę najczęściej występujących aut w tablicy cars
+//Przykład
+//dla danych wejściowych
+// Car[] _cars = new Car[]
+// {
+//     new Car(),
+//     new Car(Model: "Fiat", true),
+//     new Car(),
+//     new Car(Power: 100),
+//     new Car(Model: "Fiat", true),
+//     new Car(Power: 125),
+//     new Car()
+// };
+//wywołanie CarCounter(Car[] cars) powinno zwrócić 3
+record Car(string Model = "Audi", bool HasPlateNumber = false, int Power = 100);
+
+class Exercise3
+{
+    public static int CarCounter(Car[] cars)
+    {
+        List<Car> carsHelper = new();
+
+        foreach (Car car in cars)
+        {
+            if (carsHelper.IndexOf(car) == -1)
+            {
+                carsHelper.Add(car);
+
+            }
+        }
+        return carsHelper.Count;
+    }
+}
+
+
+record Student(string LastName, string FirstName, char Group, string StudentId = "");
+//Cwiczenie 4
+//Zdefiniuj metodę AssignStudentId, która każdemu studentowi nadaje pole StudentId wg wzoru znak_grupy-trzycyfrowy-numer.
+//Przykład
+//dla danych wejściowych
+//Student[] students = {
+//  new Student("Kowal","Adam", 'A'),
+//  new Student("Nowak","Ewa", 'A')
+//};
+//po wywołaniu metody AssignStudentId(students);
+//w tablicy students otrzymamy:
+// Kowal Adam 'A' - 'A001'
+// Nowal Ewa 'A'  - 'A002'
+//Należy przyjąc, że są tylko trzy możliwe grupy: A, B i C
+class Exercise4
+{
+    public static void AssignStudentId(Student[] students)
+    {
+        int ACounter = 1;
+        int BCounter = 1;
+        int CCounter = 1;
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            if (students[i].Group == 'A')
+            {
+                /*student.StudentId = "A00" + ACounter.ToString();*/
+                students[i] = students[i] with { StudentId = "A00" + ACounter.ToString() };
+                ACounter++;
+            }
+            else if (students[i].Group == 'B')
+            {
+                students[i] = students[i] with { StudentId = "B00" + BCounter.ToString() };
+                BCounter++;
+            }
+            else
+            {
+                students[i] = students[i] with { StudentId = "C00" + CCounter.ToString() };
+                CCounter++;
+            }
+        }
+
+
+    }
 }
